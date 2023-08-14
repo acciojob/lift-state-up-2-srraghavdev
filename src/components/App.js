ba06745836638952284a52742b4ef49acbcee170
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-14 18:16:19 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Child from './Child'
 const App = () => {
   let [inputValue,SetinputValue]=useState('')
   return (
-    <div>
+    <div className='parent'>
         <h1>Parent Component</h1>
         <div>{inputValue}</div>
         <Child state={inputValue} set={SetinputValue}/>
