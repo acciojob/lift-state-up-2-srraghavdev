bdb12d374152a1a3f1f605daae5cd805a3edc54a
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-14 18:11:24 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-
+import Child from './Child'
 const App = () => {
+  let [inputValue,SetinputValue]=useState('')
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <div>{inputValue}</div>
+        <Child state={inputValue} set={SetinputValue}/>
     </div>
   )
 }
---
+++ src/components/Child.js
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+let Child= (props)=>{
+return (
+    <div>
+        <h1>Child Component</h1>
+        <input type='text' onInput={(event)=>{props.set(event.target.value)}}></input>
+    </div>
+)
+}
+export default Child;
