5b37fadcda798f80607b719d9de213bc3407e9a5
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-14 18:13:43 +0000

    sdsd

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 let Child= (props)=>{
 return (
-    <div>
+    <div className='child'>
         <h1>Child Component</h1>
         <input type='text' onInput={(event)=>{props.set(event.target.value)}}></input>
     </div>
